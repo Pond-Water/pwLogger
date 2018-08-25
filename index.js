ae5587cc274dae61fbb2f740c0eeb15a08a1045f
Brian Carter <briancarter@chipsofttech.com>
2018-08-25 12:06:45 -0400

    add launch for tests

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-var Persistence = require('pwdb/lib/persistence')
-  , model = require('pwdb/lib/model')
-  , customUtils = require('pwdb/lib/customUtils')
-  , mkdirp = require('mkdirp')
-  , fs = require('fs')
-  , path = require('path')
-  , util = require('util')
-  ;
+var Persistence = require('pwdb/lib/persistence'),
+  model = require('pwdb/lib/model'),
+  customUtils = require('pwdb/lib/customUtils'),
+  mkdirp = require('mkdirp'),
+  fs = require('fs'),
+  path = require('path'),
+  util = require('util');
 
-function Logger (dbOptions) {  
+function Logger(dbOptions) {
   dbOptions.inMemoryOnly = false;
   dbOptions.autoload = false;
-  this.persistence = new Persistence({ db: dbOptions });
-  
+  this.persistence = new Persistence({
+    db: dbOptions
+  });
+
   // Make sure file and containing directory exist, create them if they don't
   mkdirp.sync(path.dirname(dbOptions.filename));
-  if (!fs.existsSync(dbOptions.filename)) { fs.writeFileSync(dbOptions.filename, '', 'utf8'); }
+  if (!fs.existsSync(dbOptions.filename)) {
+    fs.writeFileSync(dbOptions.filename, '', 'utf8');
+  }
 }
 
 // docs can be one document or an array of documents
 Logger.prototype.insert = function (_docs, cb) {
-  var callback = cb || function () {}
-    , docs = util.isArray(_docs) ? _docs : [_docs]
-    , preparedDocs = []
-    ;
+  var callback = cb || function () {},
+    docs = util.isArray(_docs) ? _docs : [_docs],
+    preparedDocs = [];
 
   try {
     docs.forEach(function (doc) {
       preparedDocs.push(model.deepCopy(doc));
     });
-    preparedDocs.forEach(function(doc) {
+    preparedDocs.forEach(function (doc) {
       doc._id = customUtils.uid(16);
       model.checkObject(doc);
     });
